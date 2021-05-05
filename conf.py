917b0ca38a05d88ae583f51fbaa1b382241a7271
ishan-patil2388 <ishanpatil.sip@gmail.com>
2021-05-05 12:02:44 +0530

    new fres

--- conf.py
+++ conf.py
@@ -13,20 +13,13 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import sys, os
 
-sys.path.append(os.path.abspath('_themes'))
-html_theme_path = ['_themes']
-html_theme = 'cloud'
 
 # -- Project information -----------------------------------------------------
 
-project = 'demo1'
-copyright = '2021, ishan'
-author = 'ishan'
-
-# The full version, including alpha/beta/rc tags
-release = '1.0.0'
+project = 'vipdemoiap'
+copyright = '2021, Ishan'
+author = 'Ishan'
 
 
 # -- General configuration ---------------------------------------------------
@@ -46,17 +39,12 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
-html_sidebars = { 
-    'index':    ['sidebarintro.html', 'sourcelink.html', 'searchbox.html'],
-    '**':       ['sidebarlogo.html', 'localtoc.html', 'relations.html',
-                 'sourcelink.html', 'searchbox.html']
-}
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- index.rst
+++ index.rst
@@ -1,44 +1,20 @@
-.. Python for you and me documentation master file, created by
-   sphinx-quickstart on Tue Jul  9 14:32:48 2013.
+.. vipdemoiap documentation master file, created by
+   sphinx-quickstart on Wed May  5 11:54:56 2021.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Python for you and me
-==================================
-
-This is a simple book to learn Python programming language, it is for the programmers who are new to Python.
-
-If you are new to command line in Linux, you can read `lym <https://lym.readthedocs.io/>`_.
-
-Contents:
+Welcome to vipdemoiap's documentation!
+======================================
 
 .. toctree::
    :maxdepth: 2
+   :caption: Contents:
 
-   installation
-   thebeginning
-   mu
-   variablesanddatatypes
-   operatorsexpressions
-   ifelse
-   hardwaresimulation
-   looping
-   loopingonhardware
-   datastructure
-   strings
-   functions
-   file
-   exceptions
-   classes
-   modules
-   collections
-   editors
-   pep8
-   igd
-   virtualenv
-   typehinting
-   testing
-   projectstructure
-   click
-   pypercard
-   flask
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
